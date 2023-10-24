f5f245b6b15b8d7a73bad405c6a6ea358eb94110
yalouini <yalouini07@gmail.com>
2023-10-24 15:39:40 +0200

    test recup

--- src/risk/Risk.java
+++ src/risk/Risk.java
@@ -3,10 +3,7 @@ package risk;
 public class Risk {
 
 	public static void main(String[] args) {
-		System.out.println("Le Risk fonctionne correctement");
-		System.out.println("Oui en effet");
-		System.out.println("Sur");
-		System.out.println("test lisa");
+
 	}
 
 }
