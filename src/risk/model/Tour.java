df3a87b1dc1cde5084d9e63807326804060e9248
sujedr <dejus.raphael@gmail.com>
2023-10-25 11:28:09 +0200

    Actualisation de l'affichage tour
    affichage du joueur qui est en train de jouer

--- src/risk/model/Tour.java
+++ src/risk/model/Tour.java
@@ -9,6 +9,28 @@ public class Tour {
 	private int NbRegimentsAPiocher;
 	
 	// Constructeur 
+	public Tour(int numero, Joueur joueur, Action action, int nbRegimentsAPiocher) {
+		super();
+		this.numero = numero;
+		this.joueur = joueur;
+		this.action = action;
+		NbRegimentsAPiocher = nbRegimentsAPiocher;
+	}
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
